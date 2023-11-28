8904b83480f4c9e8c755dd45cefe25f50c4e2053
tugboat-develop-19571020 <sugie@pa.ctt.ne.jp>
2023-11-28 17:28:54 +0900

    React Hooks useContext Completed

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const sugieInfo = {
+  name: "Takashi Sugie",
+  age: 66,
+};
+
+const SugieContext = createContext(sugieInfo);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <SugieContext.Provider value = {sugieInfo}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </SugieContext.Provider>
 );
 
+export default SugieContext;
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
